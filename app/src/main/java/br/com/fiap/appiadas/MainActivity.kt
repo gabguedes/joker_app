7339f4ae8a27a80bce9ef2090b713f137d51df90
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 11:51:06 -0300

    logica botao piadas

--- app/src/main/java/br/com/fiap/appiadas/MainActivity.kt
+++ app/src/main/java/br/com/fiap/appiadas/MainActivity.kt
@@ -1,14 +1,52 @@
 package br.com.fiap.appiadas
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.com.fiap.appiadas.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btTellJoker.setOnClickListener {
+
+            showJoker()
+
+        }
+    }
+
+    private fun showJoker() {
+        //Pegas as piadas do array
+        val jokers = resources.getStringArray(R.array.jokers)
+
+        //Sortear as piadas
+        val numberJoker = Random.nextInt(jokers.size)
+        val joker = jokers[numberJoker]
+
+        //mostrar a piada
+        binding.tvJoker.text = joker
+
+        playSong()
+
+    }
+
+    private fun playSong() {
+        val mediaPlayer = MediaPlayer.create(this, R.raw.badumtss)
+        mediaPlayer.start()
     }
 }
